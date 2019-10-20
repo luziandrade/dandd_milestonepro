b8f67c34885fb8abb003ba56f0a448b098a154ee
Luana Andrade <luzi015@hotmail.com>
2019-10-20 11:43:47 +0100

    fixing index not found

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'some_secret'
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '5000'), debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'),
+            port=os.getenv('PORT', '5000'),
+            debug=True)
